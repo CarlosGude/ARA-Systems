85dbd3c29559e953b3a1273a1b41d7baa3236f48
Carlos Gude <carloss.gude@gmail.com>
2020-03-10 23:14:52 +0100

    chore: Clear cache before test

--- symfony/tests/bootstrap.php
+++ symfony/tests/bootstrap.php
@@ -11,7 +11,8 @@ if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
 }
 
 // Clean database, dropping schema, execute migrations and fixtures.
-passthru(sprintf('php bin/console d:d:c --if-not-exists --env=test -q', __DIR__));
-passthru(sprintf('php bin/console d:s:d --force --full-database -q --env=test', __DIR__));
-passthru(sprintf('php bin/console d:mi:mi -n --env=test -q', __DIR__));
-passthru(sprintf('php bin/console h:f:l -n --env=test -q', __DIR__));
+passthru(sprintf('php bin/console c:c --env=test -q'));
+passthru(sprintf('php bin/console d:d:c --if-not-exists --env=test -q'));
+passthru(sprintf('php bin/console d:s:d --force --full-database -q --env=test'));
+passthru(sprintf('php bin/console d:mi:mi -n --env=test -q'));
+passthru(sprintf('php bin/console h:f:l -n --env=test -q'));
