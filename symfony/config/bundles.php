a298f9c78dbe0a9ba2be99f74e6ad82baa813a26
Carlos Gude Sánchez <carlos.sgude@gmail.com>
2020-03-16 18:51:04 +0100

    feat: Add vich uploader
    Signed-off-by: Carlos Gude Sánchez <carlos.sgude@gmail.com>

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
