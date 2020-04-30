c604ac1cb111a55dc240dbc9c91336ffe4995652
Carlos Gude Sánchez <carlos.sgude@gmail.com>
2020-04-30 13:23:06 +0200

    feat: Add personalized login

--- symfony/src/Controller/SecurityController.php
+++ symfony/src/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Company;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,12 +11,16 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/", name="app_login")
+     * @Route("/{company}", name="app_login")
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, string $company = null): Response
     {
         if ($this->getUser()) {
             return $this->redirectToRoute('front_dashboard');
+        }
+
+        if($company){
+            $company = $this->getDoctrine()->getRepository(Company::class)->findOneBy(['slug'=>$company]);
         }
 
         // get the login error if there is one
@@ -23,6 +28,10 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'company' => $company,
+            'error' => $error
+        ]);
     }
 }
