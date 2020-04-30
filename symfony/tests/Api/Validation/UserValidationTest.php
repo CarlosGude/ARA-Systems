d23224a98a8d57214777d8199b676beae870faa6
Carlos Gude Sánchez <carlos.sgude@gmail.com>
2020-04-30 18:05:27 +0200

    test: Fix user validation

--- symfony/tests/Api/Validation/UserValidationTest.php
+++ symfony/tests/Api/Validation/UserValidationTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Api\Validation;
 
+use App\Entity\User;
 use App\Tests\Api\BaseTest;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
@@ -32,13 +33,18 @@ class UserValidationTest extends BaseTest
      */
     public function testNameRequired(): void
     {
-        $company = ['user' => parent::API.'users/'.$this->getGodUser()->getId()];
+        $user = [
+            'email' => 'fake2@email.com',
+            'profile' => User::PROFILE_ADMIN,
+            'password' => 'password',
+            'company' => parent::API.'companies/'.$this->getCompany('Another Company')->getId(),
+        ];
 
         $response = static::createClient()->request(
             'POST',
-            parent::API.'companies',
+            parent::API.'users',
             ['headers' => ['Authorization' => 'Bearer '.$this->token['token'], 'Content-Type' => 'application/json'],
-                'body' => json_encode($company),
+                'body' => json_encode($user),
             ]
         );
         $response = json_decode($response->getBrowserKitResponse()->getContent(), true);
@@ -46,6 +52,208 @@ class UserValidationTest extends BaseTest
         self::assertResponseStatusCodeSame(400, 'The response is not 204');
         $this->assertEquals(
             'name: Este valor no debería estar vacío.',
+            $response['hydra:description']
+        );
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     */
+    public function testEmailRequired(): void
+    {
+        $user = [
+            'name' => 'name',
+            'profile' => User::PROFILE_ADMIN,
+            'password' => 'password',
+            'company' => parent::API.'companies/'.$this->getCompany('Another Company')->getId(),
+        ];
+
+        $response = static::createClient()->request(
+            'POST',
+            parent::API.'users',
+            ['headers' => ['Authorization' => 'Bearer '.$this->token['token'], 'Content-Type' => 'application/json'],
+                'body' => json_encode($user),
+            ]
+        );
+        $response = json_decode($response->getBrowserKitResponse()->getContent(), true);
+
+        self::assertResponseStatusCodeSame(400, 'The response is not 204');
+        $this->assertEquals(
+            'email: Este valor no debería estar vacío.',
+            $response['hydra:description']
+        );
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     */
+    public function testProfileRequired(): void
+    {
+        $user = [
+            'name' => 'name',
+            'email' => 'fake2@email.com',
+            'password' => 'password',
+            'company' => parent::API.'companies/'.$this->getCompany('Another Company')->getId(),
+        ];
+
+        $response = static::createClient()->request(
+            'POST',
+            parent::API.'users',
+            ['headers' => ['Authorization' => 'Bearer '.$this->token['token'], 'Content-Type' => 'application/json'],
+                'body' => json_encode($user),
+            ]
+        );
+        $response = json_decode($response->getBrowserKitResponse()->getContent(), true);
+
+        self::assertResponseStatusCodeSame(400, 'The response is not 204');
+        $this->assertEquals(
+            'profile: Este valor no debería estar vacío.',
+            $response['hydra:description']
+        );
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     */
+    public function testPasswordRequired(): void
+    {
+        $user = [
+            'name' => 'name',
+            'email' => 'fake2@email.com',
+            'profile' => User::PROFILE_ADMIN,
+            'company' => parent::API.'companies/'.$this->getCompany('Another Company')->getId(),
+        ];
+
+        $response = static::createClient()->request(
+            'POST',
+            parent::API.'users',
+            ['headers' => ['Authorization' => 'Bearer '.$this->token['token'], 'Content-Type' => 'application/json'],
+                'body' => json_encode($user),
+            ]
+        );
+        $response = json_decode($response->getBrowserKitResponse()->getContent(), true);
+
+        self::assertResponseStatusCodeSame(400, 'The response is not 204');
+        $this->assertEquals(
+            'password: Este valor no debería estar vacío.',
+            $response['hydra:description']
+        );
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     */
+    public function testCompanyRequired(): void
+    {
+        $user = [
+            'name' => 'name',
+            'email' => 'fake2@email.com',
+            'profile' => User::PROFILE_ADMIN,
+            'password' => 'password',
+        ];
+
+        $response = static::createClient()->request(
+            'POST',
+            parent::API.'users',
+            ['headers' => ['Authorization' => 'Bearer '.$this->token['token'], 'Content-Type' => 'application/json'],
+                'body' => json_encode($user),
+            ]
+        );
+        $response = json_decode($response->getBrowserKitResponse()->getContent(), true);
+
+        self::assertResponseStatusCodeSame(400, 'The response is not 204');
+        $this->assertEquals(
+            'company: Este valor no debería estar vacío.',
+            $response['hydra:description']
+        );
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     */
+    public function testProfileValidRequired(): void
+    {
+        $user = [
+            'name' => 'name',
+            'email' => 'fake2@email.com',
+            'profile' => 'profile',
+            'password' => 'password',
+            'company' => parent::API.'companies/'.$this->getCompany('Another Company')->getId(),
+        ];
+
+
+        $response = static::createClient()->request(
+            'POST',
+            parent::API.'users',
+            ['headers' => ['Authorization' => 'Bearer '.$this->token['token'], 'Content-Type' => 'application/json'],
+                'body' => json_encode($user),
+            ]
+        );
+        $response = json_decode($response->getBrowserKitResponse()->getContent(), true);
+
+        self::assertResponseStatusCodeSame(400, 'The response is not 204');
+        $this->assertEquals(
+            'profile: El valor seleccionado no es una opción válida.',
+            $response['hydra:description']
+        );
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     */
+    public function testEmailValid(): void
+    {
+        $user = [
+            'name' => 'name',
+            'email' => 'fake',
+            'profile' => User::PROFILE_ADMIN,
+            'password' => 'password',
+            'company' => parent::API.'companies/'.$this->getCompany('Another Company')->getId(),
+        ];
+
+
+        $response = static::createClient()->request(
+            'POST',
+            parent::API.'users',
+            ['headers' => ['Authorization' => 'Bearer '.$this->token['token'], 'Content-Type' => 'application/json'],
+                'body' => json_encode($user),
+            ]
+        );
+        $response = json_decode($response->getBrowserKitResponse()->getContent(), true);
+
+        self::assertResponseStatusCodeSame(400, 'The response is not 204');
+        $this->assertEquals(
+            'email: Este valor no es una dirección de email válida.',
+            $response['hydra:description']
+        );
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     */
+    public function testEmailUnique(): void
+    {
+        $user = [
+            'name' => 'name',
+            'email' => 'carlos.sgude@gmail.com',
+            'profile' => User::PROFILE_ADMIN,
+            'password' => 'password',
+            'company' => parent::API.'companies/'.$this->getCompany('Another Company')->getId(),
+        ];
+
+
+        $response = static::createClient()->request(
+            'POST',
+            parent::API.'users',
+            ['headers' => ['Authorization' => 'Bearer '.$this->token['token'], 'Content-Type' => 'application/json'],
+                'body' => json_encode($user),
+            ]
+        );
+        $response = json_decode($response->getBrowserKitResponse()->getContent(), true);
+
+        self::assertResponseStatusCodeSame(400, 'The response is not 204');
+        $this->assertEquals(
+            'email: Este valor ya se ha utilizado.',
             $response['hydra:description']
         );
     }
