a3728d11fc36a8de7108663520c17549d82d1242
Carlos Gude Sánchez <carlos.sgude@gmail.com>
2020-05-07 11:40:25 +0200

    test: Add catalog basic tests

---
+++ symfony/tests/Front/Management/Admin/CatalogManagementTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Tests\Front\Roles\Admin;
+
+use App\Entity\Category;
+use App\Entity\Product;
+use App\Tests\Front\BaseTest;
+use Symfony\Component\HttpFoundation\Response;
+
+class CatalogManagementTest extends BaseTest
+{
+    public function testShowCatalog(): void
+    {
+        $client = $this->login(parent::LOGIN_ADMIN);
+
+        $client->request('GET', $this->generatePath('front_list',['entity'=>'product','view'=>'catalog']));
+
+        self::assertResponseIsSuccessful();
+
+    }
+
+    public function testShowProduct(): void
+    {
+        $client = $this->login(parent::LOGIN_ADMIN);
+
+        /** @var Product $categoryToEdit */
+        $productToShow = $this->getRepository(Product::class)->findOneBy([
+            'name' => 'Product 1',
+            'company' => $this->getCompany('Another Company'),
+        ]);
+        $client->request('GET', $this->generatePath('front_list',['entity'=>'product','view'=>'catalog']));
+
+        $url = $this->generatePath('front_edit', [
+            'entity' => 'product',
+            'id' => $productToShow->getId(),
+            'view'=>'show'
+        ]);
+        $client->request('GET',$url);
+
+        self::assertResponseIsSuccessful();
+    }
+
+}
